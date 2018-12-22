83d634c65d42f206ccf4c0bf672b8ae4a36d9168
naorcohen <naorcohen86@gmail.com>
2018-12-22 22:50:41 +0200

    blinking changes

--- blinking SOS/src/main.cpp
+++ blinking SOS/src/main.cpp
@@ -1,12 +1,37 @@
 #include <Arduino.h>
 
 void setup() {
-  pinMode(2, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(2, HIGH);
-  delay(1000);
-  digitalWrite(2, LOW);
-  delay(1000);
+
+   int a=0;
+
+   for(a=0 ;a <10; a++)
+   {
+     digitalWrite(LED_BUILTIN, HIGH);
+     delay(1000);
+     digitalWrite(LED_BUILTIN, LOW);
+     delay(1000);
+   }
+
+   for(a=0 ;a <10 ;a++)
+   {
+     digitalWrite(LED_BUILTIN, HIGH);
+     delay(500);
+     digitalWrite(LED_BUILTIN, LOW);
+     delay(500);
+   }
+
+   for(a=0 ;a <10; a++)
+   {
+     digitalWrite(LED_BUILTIN, HIGH);
+     delay(100);
+     digitalWrite(LED_BUILTIN, LOW);
+     delay(100);
+   }
+
+
+
 }
