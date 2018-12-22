3ea407b0ec2ce8dc2e6f1a9914bb11108bce5965
naorcohen <naorcohen86@gmail.com>
2018-12-22 23:53:32 +0200

    with time stamp

--- blinking SOS/src/main.cpp
+++ blinking SOS/src/main.cpp
@@ -1,22 +1,31 @@
 #include <Arduino.h>
 
 void setup() {
+   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-
+   int time = micros();
+   static int counterLoop=0;
+   Serial.printf("loop #%d. time=%d\n", counterLoop,time);
+   counterLoop++;
    int a=0;
 
-   for(a=0 ;a <10; a++)
+   for(a=0 ;a <2; a++)
    {
+      int time1 = micros();
+       digitalWrite(LED_BUILTIN, HIGH);
+      int time2 = micros();
+      Serial.printf("before. time=%d\n", time1);
+      Serial.printf("after. time=%d\n", time2);
      digitalWrite(LED_BUILTIN, HIGH);
      delay(1000);
      digitalWrite(LED_BUILTIN, LOW);
      delay(1000);
    }
 
-   for(a=0 ;a <10 ;a++)
+   for(a=0 ;a <2 ;a++)
    {
      digitalWrite(LED_BUILTIN, HIGH);
      delay(500);
@@ -26,6 +35,7 @@ void loop() {
 
    for(a=0 ;a <10; a++)
    {
+      //Serial.println("i am the first project. time=%d", time);
      digitalWrite(LED_BUILTIN, HIGH);
      delay(100);
      digitalWrite(LED_BUILTIN, LOW);
